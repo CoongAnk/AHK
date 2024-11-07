965091200f2fb18fdaf734119c769df377bbb7aa
CoongAnk <conganh8864@gmail.com>
2024-11-07 22:13:13 +0700

    07/11/2024

--- A-TrangChu/TrangChu-JS/ChuyenDongFeedback02.js
+++ A-TrangChu/TrangChu-JS/ChuyenDongFeedback02.js
@@ -1,28 +1,27 @@
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('.news-carousel').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: true,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+$(document).ready(function(){
+    $('.news-carousel').slick({
+        slidesToShow: 3, // Hiển thị 3 ảnh trên màn hình lớn
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1200, // Dưới 1024px
+                settings: {
+                    slidesToShow: 2, // Hiển thị 2 ảnh
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
                 }
-            ]
-        })
+            },
+            {
+                breakpoint: 800, // Dưới 600px
+                settings: {
+                    slidesToShow: 1, // Hiển thị 1 ảnh
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
-</script>
+});
+
